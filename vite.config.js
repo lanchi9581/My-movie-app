4a60c6086bca58676cd46e91985eebbb848e03bf
Lanchi9581 <lan.zvajker@gmail.com>
2025-10-22 12:33:05 +0200

    The URL should work on localhost

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/My-movie-app/',
+  base: '/My-movie-app',
   build: {
     outDir: 'docs',
   },
